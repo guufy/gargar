558c2f424adb8ba45f28ef58592925e59990911f
Joseph Tang <josephtang765@gmail.com>
2019-07-20 21:54:38 +0800

    Use new send methods

--- bot_shapiro00.py
+++ bot_shapiro00.py
@@ -9,41 +9,42 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
+    print(message)
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
     if message.content.startswith('?hello'):
         msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if message.content == ('i hate'):
         msg = 'mondays'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if random.randint(0,1000) < 1:
         msg = 'wheres my pipe'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if ('shid') in message.content:
         msg = 'ofuk {0.author.mention} shidded'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if ('camed') in message.content:
         msg = 'ofuk {0.author.mention} camed'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if ('fard') in message.content:
         msg = 'ofuk {0.author.mention} farded'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if ('pee') in message.content:
         msg = 'ofuk {0.author.mention} peed'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if message.content == ('?help'):
         msg = '?hello\nshid\ncamed\nfard\npee'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
     if message.content == ("?gargar"):
         await client.send_file(message.channel,"gargar/" + str(random.randint(1,174)) + ".png")
